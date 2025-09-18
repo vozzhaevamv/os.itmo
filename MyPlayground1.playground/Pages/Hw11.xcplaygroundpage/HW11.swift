15b5468c15e0abf51077a4d4fd06b1fa0a1818bb
vozzhaevamv <mashazhukova2311@gmail.com>
2025-09-18 14:35:29 +0900

    Update and rename Contents.swift to HW11.swift

--- MyPlayground1.playground/Pages/Hw11.xcplaygroundpage/HW11.swift
+++ MyPlayground1.playground/Pages/Hw11.xcplaygroundpage/HW11.swift
@@ -2,8 +2,7 @@ import UIKit
 
 let ican: String = "i can"
 let code: String = "code"
-let final: String = ican + " " + code
+let final: String = ican + " " + code + "!"
 print (final)
-let final2: String = "\(ican) \(code)"
+let final2: String = "\(ican) \(code)!"
 print (final2)
-
